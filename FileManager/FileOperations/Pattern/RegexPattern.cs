b344c902bbf3e693fd778392ba5ae7f318f5bd47
walter-eidi <walter-eidi@hotmail.com>
2025-04-20 20:06:47 -0300

    Corrigido parametrização entre paginas de selecionar arquivo e renderizar arquivo.

--- FileManager/FileOperations/Pattern/RegexPattern.cs
+++ FileManager/FileOperations/Pattern/RegexPattern.cs
@@ -1,10 +1,5 @@
 ﻿using FileManager.DAO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace FileManager.FileOperations.Pattern
 {
